b8b0eb5e7719f06be77466555467264b534fd6c0
Nicole Pasquale <NSquale212@gmail.com>
2021-02-03 21:48:44 -0600

    Add svg icons to footer

--- src/client/components/Footer.tsx
+++ src/client/components/Footer.tsx
@@ -1,34 +1,30 @@
 import * as React from 'react';
 import { FaRegCopyright } from 'react-icons/fa';
 import { Jumbotron, Nav } from 'react-bootstrap';
+import Github from './svgs/Github';
+import LinkedIn from './svgs/LinkedIn';
 
-const Footer: React.FC<IFooterProps> = () => {
+
+const Footer = () => {
 	return (
 		<Jumbotron className="mb-0 mt-5 footer" fluid>
 			<div className="row flex-column justify-content-center align-items-center">
-				<h6>Copyright<FaRegCopyright /> 2020 Squale Chronicles</h6>
-			
-			<Nav activeKey="/home" onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
-				<Nav.Item>
-					<Nav.Link href="/home">Active</Nav.Link>
-				</Nav.Item>
-				<Nav.Item>
-					<Nav.Link eventKey="link-1">Link</Nav.Link>
-				</Nav.Item>
-				<Nav.Item>
-					<Nav.Link eventKey="link-2">Link</Nav.Link>
-				</Nav.Item>
-				<Nav.Item>
-					<Nav.Link eventKey="disabled" disabled>
-						Disabled
-					</Nav.Link>
-				</Nav.Item>
-			</Nav>
+				<Nav activeKey="/home" onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
+					<Nav.Item>
+						<Nav.Link href="https://github.com/NSquale21" target="_blank">
+							<Github />
+						</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link href="https://www.linkedin.com/in/nicole-pasquale/" target="_blank">
+							<LinkedIn />
+						</Nav.Link>
+					</Nav.Item>
+				</Nav>
+				<h6 className="dark-gray"><FaRegCopyright />Copyright 2021 Squale Chronicles</h6>
 			</div>
 		</Jumbotron>
 	);
 };
-
-export interface IFooterProps {}
 
 export default Footer;
